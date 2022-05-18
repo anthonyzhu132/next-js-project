ec04d1d32331280968c66c99247ff1eaa4e12e7c
Anthony Zhu <anthonyzhu132@gmail.com>
2022-05-18 13:04:07 -0700

    fixed code where it was giving error 500

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,16 +1,24 @@
 import { MongoClient } from "mongodb";
 // api/new-meetup
 
-
-
-export default function handler(req, res) {
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
 
-    const { title, image, address, description } = data;
-
-    MongoClient.connect(
+    const client = await MongoClient.connect(
       `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.m29e0.mongodb.net/?retryWrites=true&w=majority`
     );
+    const db = client.db();
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "success" });
   }
 }
+
+export default handler;
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,8 +1,18 @@
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
 export default function NewMeetupPage() {
-  const addMeetup = (formData) => {
-    console.log(formData);
+  const addMeetup = async (formData) => {
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(formData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+
+    console.log(data);
   };
 
   return <NewMeetupForm onAddMeetup={addMeetup} />;
