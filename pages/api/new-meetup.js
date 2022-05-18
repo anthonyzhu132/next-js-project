6b6f18166d990badfa3234ca1ded7e2dd4a125e4
Anthony Zhu <anthonyzhu132@gmail.com>
2022-05-18 12:42:48 -0700

    added mongodb

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,4 +1,7 @@
+import { MongoClient } from "mongodb";
 // api/new-meetup
+
+
 
 export default function handler(req, res) {
   if (req.method === "POST") {
@@ -6,6 +9,8 @@ export default function handler(req, res) {
 
     const { title, image, address, description } = data;
 
-    
+    MongoClient.connect(
+      `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.m29e0.mongodb.net/?retryWrites=true&w=majority`
+    );
   }
 }
